96297208f88df1ee7293ae0097581514b9e6e2ee
roy <roy93group@gmail.com>
2023-09-02 16:03:32 +0700

    update

---
+++ app/src/main/java/com/roy/calculator/MyApplication.kt
@@ -0,0 +1,26 @@
+package com.roy.calculator
+
+import android.app.Application
+
+//TODO firebase
+//TODO ad
+//TODO  ad id manifest
+//TODO leak canary
+//TODO ic_launcher
+//TODO policy
+//TODO rate app
+//TODO share app
+//TODO more app
+//TODO proguard
+//TODO keystore
+
+//done
+//pkg name
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
